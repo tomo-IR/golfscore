7974805ebfcb06cec4b738cc86f23749a25279c5
tomo-IR <sunnyday20200101@gmail.com>
2020-12-20 11:23:25 +0900

    horoku

--- db/migrate/20201016131954_devise_create_users.rb
+++ db/migrate/20201016131954_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false
+      # t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      # t.string :name
+      t.string :name
       t.timestamps null: false
     end
 
