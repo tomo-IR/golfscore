834f0fb7a4e13130a17af3a16a592cfcbd0019d5
tomo-IR <sunnyday20200101@gmail.com>
2021-01-03 21:48:00 +0900

    coursename_save

--- app/controllers/golfcourses_controller.rb
+++ app/controllers/golfcourses_controller.rb
@@ -28,8 +28,8 @@ class GolfcoursesController < ApplicationController
 
     
     #2.全てのゴルフ場情報をループさせて取得
-    # (1..3).each do|page|
-    (1.. pagecount.to_i).each do|page|
+    (1..3).each do|page|
+    # (1.. pagecount.to_i).each do|page|
       uri = URI(base_url + '?' + params.map { |k,v| "#{k}=#{v}" }.join('&') + "&page=" + page.to_s)
       puts uri
       response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
@@ -42,14 +42,31 @@ class GolfcoursesController < ApplicationController
       @json = JSON.parse(response.body)
       # puts json
       json["Items"].each do|name|
-        puts name["Item"]["golfCourseName"]
-        golfcourse = Golfcourse.new(:golfcoursename => name["Item"]["golfCourseName"], :golfcourseid => name["Item"]["golfCourseId"])
-        puts name["Item"]["golfCourseId"]
-        if golfcourse.save 
-            # redirect_to root_path
-        else
-            flash.now[:danger] = '保存できませんでした'
+        # golfcourse = Golfcourse.new(:golfcoursename => name["Item"]["golfCourseName"],
+                                    #  :golfcourseid => name["Item"]["golfCourseId"])
+        puts "DB側のgolfcourseIDは" + Golfcourse.find_by(golfcourseid: name["Item"]["golfCourseId"]).golfcourseid.to_s
+        puts "API側のgolfcourseIDは" + name["Item"]["golfCourseId"].to_s
+        
+        puts "DB側のgolfcourseNameは" + Golfcourse.find_by(golfcourseid: name["Item"]["golfCourseId"]).golfcoursename.to_s
+        puts "API側のgolfcourseNameは" + name["Item"]["golfCourseName"].to_s
+        
+        if (Golfcourse.find_by(golfcourseid: name["Item"]["golfCourseId"]).golfcourseid.to_s == name["Item"]["golfCourseId"].to_s) && (Golfcourse.find_by(golfcourseid: name["Item"]["golfCourseId"]).golfcoursename.to_s == name["Item"]["golfCourseName"].to_s)
+          puts "idも存在していて、nameの変更もないので、なにもしません。"
+        elsif (Golfcourse.find_by(golfcourseid: name["Item"]["golfCourseId"]).golfcourseid.to_s == name["Item"]["golfCourseId"].to_s) && (Golfcourse.find_by(golfcourseid: name["Item"]["golfCourseId"]).golfcoursename.to_s != name["Item"]["golfCourseName"].to_s)
+            puts "idは存在しているが、nameが変更されているので、更新します"
+        elsif Golfcourse.find_by(golfcourseid: name["Item"]["golfCourseId"]).golfcourseid.to_s == name["Item"]["golfCourseId"].to_s
+          # .create
+          puts "idが存在しなかったので、新規保存します"
         end
+
+
+        # 保存の処理
+        # if golfcourse.save 
+        #     # redirect_to root_path
+        # else
+        #     flash.now[:danger] = '保存できませんでした'
+        # end
+
       end
     end
   end 
