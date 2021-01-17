e452084af8fec311634e21f057a464abbcf81a02
tomo-IR <sunnyday20200101@gmail.com>
2021-01-17 10:41:39 +0900

    message_ajax_complete

--- app/controllers/golfcourses_controller.rb
+++ app/controllers/golfcourses_controller.rb
@@ -70,7 +70,8 @@ class GolfcoursesController < ApplicationController
 
     # メッセージボード関係
 		@message = Message.new
-    @messages = Message.where(golfcourse_id: playing_course.golfcourse_id)#.includes([:user])#.order(created_at: "DESC")
+    @messages = Message.where(golfcourse_id: playing_course.golfcourse_id).order(created_at: "DESC").page(params[:page]).per(10)
+  
   end
 
   def finish
