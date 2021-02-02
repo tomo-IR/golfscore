3dc78d362d54b2049ad21b933964f4740496775a
tomo-IR <sunnyday20200101@gmail.com>
2021-02-02 18:00:48 +0900

    top-timeline

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,32 @@ class HomeController < ApplicationController
                               .where(status: 0)
                               .includes([:golfcourse])
     end
+
+    @user = User.find(current_user.id)
+    @following_user = @user.followings
+    @array = []
+    @following_user.each do|score|
+      @array.push(score.id)
+    end
+
+    from  = Time.current.at_beginning_of_day + 1.day
+    to    = (from - 7.day).at_end_of_day
+
+    @following_score = Score.where(user_id: @array)
+                            .where(published: 1)
+                            .where(played_date: to...from) 
+                            .order(played_date: :desc)
+                            .includes([:user])
+                            .includes([:golfcourse])
+
+    
+
+    # @following_score = Score.where(user_id: @following_user.id)
+    
+    
+
+
+
   end
 
   def authentication
@@ -25,4 +51,7 @@ class HomeController < ApplicationController
     sign_in user
     redirect_to root_path, :notice => 'ゲストプレイヤーとしてログインしました。'
   end
+
+
+
 end
