e18f40f8ea0c67b1462a1220856e95af92e43790
tomo-IR <sunnyday20200101@gmail.com>
2021-02-06 22:25:43 +0900

    revert2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,28 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+<<<<<<< HEAD
 
+=======
+//= require jquery
+//= require jquery_ujs
+>>>>>>> 8c38a4b35fea74dfaefa2fa1bb73b5fcdb65b5df
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+<<<<<<< HEAD
 <<<<<<< HEAD
 // import "../src/css/application"
 import 'bootstrap';
 import '../stylesheets/application.scss';
 =======
 >>>>>>> parent of 431c7f2... 2-6
+=======
+// import "../src/css/application"
+import 'bootstrap';
+import '../stylesheets/application.scss';
+>>>>>>> 8c38a4b35fea74dfaefa2fa1bb73b5fcdb65b5df
 
 Rails.start()
 Turbolinks.start()
