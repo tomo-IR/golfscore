8c38a4b35fea74dfaefa2fa1bb73b5fcdb65b5df
tomo-IR <sunnyday20200101@gmail.com>
2021-02-06 13:02:16 +0900

    revert-test

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+=======
+// require jquery
+// require jquery_ujs
+// require turbolinks
+// require_tree .
+
+>>>>>>> parent of 3e65ddd... js-error
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -10,10 +11,23 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 // import "../src/css/application"
 import 'bootstrap';
-import '../stylesheets/application';
+import '../stylesheets/application.scss';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+=======
+// require jquery
+// require jquery_ujs
+// require turbolinks
+// require_tree 
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+>>>>>>> parent of 3e65ddd... js-error
 
 console.log("hoge");
