bef690478e0664c8ddcd0b4b75aaa1b79c707037
tomo-IR <sunnyday20200101@gmail.com>
2021-02-07 14:39:18 +0900

    js-complete

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 // 追加
-require('user/sample')
+// require('user/sample')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/user/sample.js
+++ app/javascript/user/sample.js
@@ -1,3 +1,8 @@
-window.onload = function() {
-    alert("hoge");
-};
+console.log("hoge")
+
+document.addEventListener("DOMContentLoaded", function() {
+    var hoge = document.getElementById("eagle");
+    hoge.addEventListener('click', () => {
+        console.log('Hello');
+    });
+}, false);
