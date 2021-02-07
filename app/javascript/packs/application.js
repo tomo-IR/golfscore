435f4364d04aabf3403e17046bae5776b821633b
tomo-IR <sunnyday20200101@gmail.com>
2021-02-07 11:58:33 +0900

    add-js-sample

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// 追加
+require('user/sample')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,11 +17,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require jquery
-//= require jquery_ujs
 
-console.log("hoge");
-
-document.getElementById("eagle").onclick = function() {
-    console.log("hoge");
-};
+console.log("hoge")
