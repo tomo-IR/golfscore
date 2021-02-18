2875dcc79862118975e672ac2b9c73ec79a064ad
tomo-IR <sunnyday20200101@gmail.com>
2021-02-19 05:32:36 +0900

    whenever-production

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,7 @@
-set :output, 'log/crontab.log'
-set :environment, :production
-# Rails.rootを使用するために必要
 require File.expand_path(File.dirname(__FILE__) + "/environment")
-# cronを実行する環境変数
-rails_env = ENV['RAILS_ENV'] || :development
-# cronを実行する環境変数をセット
-set :environment, rails_env
-# cronのログの吐き出し場所
-set :output, "#{Rails.root}/log/cron.log"
+set :environment, :production
+set :output, "#{Rails.root}/log/cron.log" 
+
 
 every 5.minute do
   rake "get_golfcourse_info:get"
