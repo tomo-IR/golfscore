36e783670984b78e0c1ce9cffdbff30a7e8f57b3
tomo-IR <sunnyday20200101@gmail.com>
2021-02-18 10:41:25 +0900

    whenever-production

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, 'log/crontab.log'
-set :environment, ENV['RAILS_ENV']
+set :environment, :production
 # Rails.rootを使用するために必要
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 # cronを実行する環境変数
